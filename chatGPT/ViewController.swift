a962f98e1906efba28a3dd5ce95a3d609bc8c3c1
Georgiy-N <neguritsageorgiy@gmail.com>
2023-04-12 20:31:27 +0600

    Initial Commit

---
+++ chatGPT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chatGPT
+//
+//  Created by Георгий Негурица on 12/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
